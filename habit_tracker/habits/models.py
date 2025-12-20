d96b8e69d713d93d8730b16880eae84993b2d9fd
Your Name <you@example.com>
2025-12-20 23:03:54 +0800

    описал модель привычки

--- habit_tracker/habits/models.py
+++ habit_tracker/habits/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Habit(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='habits')
+    action = models.CharField(max_length=255)
+    place = models.CharField(max_length=255)
+    time = models.TimeField()
+    is_reward = models.BooleanField(default=False)
+    related_habit = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='related_to')
+    reward = models.CharField(max_length=255, null=True, blank=True)
+    periodicity = models.PositiveSmallIntegerField(default=1)
+    duration = models.PositiveSmallIntegerField()
+    is_public = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.action} ({self.user.username})"
+
