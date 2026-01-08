ea3d43805a21fa5087ebc391da6085e9ca7b1f78
slavakps <slavakpss99@bk.ru>
2026-01-08 19:00:59 +0800

    Update views.py

--- habit_tracker/users/views.py
+++ habit_tracker/users/views.py
@@ -1,9 +1,8 @@
-from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth import get_user_model
 from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-
 from .serializers import LoginSerializer, RegisterSerializer
 
 User = get_user_model()
